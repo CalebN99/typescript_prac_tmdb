446271f0a0aeef873c7d309e2b036f7631a02e0d
Caleb Norris <paintballerz15@gmail.com>
2023-07-29 11:30:47 -0700

    React setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/App.css';
+import NavB from "./components/NavB";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavB />
+      <h1>Movies</h1>
+   
+    </div>
+  );
+}
+
+export default App;
