e089a3ef38fce3fb8698f7ce62ed2da7eb24b18e
Caleb Norris <paintballerz15@gmail.com>
2023-07-29 19:34:14 -0700

    Movie card design, and movie grid built

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,27 @@ import NavB from "./components/NavB";
 import Movies from "./components/Movies";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Spin } from 'antd';
+
+
 function App() {
 
   let [data, setData] = useState(0);
 
 
   useEffect(() => {
-    try {
-      fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key=742b4f9ef0f1842c765810ec16e2e95a"
-      )
-        .then((res) => res.json())
-        .then(setData);
-    } catch(err) {
-      console.log(err);
-    }
+    setTimeout(() => {
+      try {
+        fetch(
+          "https://api.themoviedb.org/3/discover/movie?api_key=742b4f9ef0f1842c765810ec16e2e95a"
+        )
+          .then((res) => res.json())
+          .then(setData);
+      } catch(err) {
+        console.log(err);
+      }
+    }, 1000)
+ 
      
   }, []);
 
@@ -27,9 +33,8 @@ function App() {
     <div className="App">
       <NavB />
       {
-        data !== 0 ? <Movies movies={data} /> : null
+        data !== 0 ? <Movies movies={data} /> : <Spin size="large"/>
       }
-     
       
     </div>
   );
--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,14 +1,60 @@
 import React from "react";
 import "../styles/App.css";
+import { DownOutlined, UserOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Button, Dropdown, message, Space, Tooltip } from "antd";
 
-function Movies(props:any) {
+function Movies(props: any) {
+  console.log(props.movies);
 
-    console.log(props.movies);
-  
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: <p>Action</p>,
+    },
+    {
+      key: "2",
+      label: <p>Comedy</p>,
+    },
+    {
+      key: "3",
+      label: <p>Adventure</p>,
+    },
+    {
+      key: "4",
+      label: <p>Horror</p>,
+    },
+    {
+      key: "5",
+      label: <p>Drama</p>,
+    },
+  ];
+
   return (
-   <div className="Movies">
-    <h1>Movies</h1>
-   </div>
+    <div className="Movies">
+      <Dropdown className="genre_dropdown" menu={{ items }}>
+        <Button className="genre_dropdown">
+          <Space className="genre_dropdown">
+            Button
+            <DownOutlined />
+          </Space>
+        </Button>
+      </Dropdown>
+      <div className="movie_card_grid">
+        {props.movies.results.map((movie: any) => {
+          return (
+            <div className="movie_card">
+              <img
+                src={"https://image.tmdb.org/t/p/w300/" + movie.poster_path}
+                className="movie_image_small"
+                alt="movie poster"
+              />
+              <p className="movie_title">{movie.title}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
